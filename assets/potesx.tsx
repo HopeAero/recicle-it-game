79b575b2d11cdebd551d5bf8985840b31fd9232d
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2024-11-05 01:30:56 -0400

    terminando el pase pa salmon humon

---
+++ assets/potesx.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="potesx" tilewidth="32" tileheight="32" tilecount="225" columns="15">
+ <image source="recicle.png" width="499" height="499"/>
+</tileset>
---
+++ assets/recicleIt.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="recicleIt" tilewidth="32" tileheight="32" tilecount="16638" columns="141">
+ <image source="Modern_Exteriors_Complete_Tileset_32x32.png" width="4512" height="3776"/>
+</tileset>
